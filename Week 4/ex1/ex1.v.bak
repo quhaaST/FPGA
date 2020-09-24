7cbf19b6b50fe2d2bdc7292f660d72758718c237
quhaast <asndjabbd@gmail.com>
2020-09-24 15:22:49 +0300

    Week 4 tasks

---
+++ Week 4/ex1/ex1.v.bak
@@ -0,0 +1,5 @@
+module ex1(SW0, SW1, LED0);
+	input SW0, SW1;
+	output LED0;
+	and(LED0, SW0, SW1);
+endmodule
---
+++ Week 4/ex2/ex2.v
@@ -0,0 +1,48 @@
+module ex2
+(
+	input [1:0] KEY,
+	output [47:0] HEX
+);
+//10.04.02
+genvar j;
+generate
+	for (j = 0; j < 6; j = j + 1)
+		begin: gen2
+			case(j)
+				0: assign HEX[1]=~KEY[0],
+							HEX[2]=~KEY[0];
+				1: assign HEX[0+j*8]=~KEY[0],
+							HEX[1+j*8]=~KEY[0],
+							HEX[2+j*8]=~KEY[0],
+							HEX[3+j*8]=~KEY[0],
+							HEX[4+j*8]=~KEY[0],
+							HEX[5+j*8]=~KEY[0],
+							HEX[7+j*8]=~KEY[0];
+				2: assign HEX[0+j*8]=~KEY[0],
+							HEX[1+j*8]=~KEY[0],
+							HEX[2+j*8]=~KEY[0],
+							HEX[3+j*8]=~KEY[0],
+							HEX[4+j*8]=~KEY[0],
+							HEX[5+j*8]=~KEY[0];
+				3: assign HEX[1+j*8]=~KEY[0],
+							HEX[2+j*8]=~KEY[0],
+							HEX[5+j*8]=~KEY[0],
+							HEX[6+j*8]=~KEY[0],
+							HEX[7+j*8]=~KEY[0];
+				4: assign HEX[0+j*8]=~KEY[0],
+							HEX[1+j*8]=~KEY[0],
+							HEX[2+j*8]=~KEY[0],
+							HEX[3+j*8]=~KEY[0],
+							HEX[4+j*8]=~KEY[0],
+							HEX[5+j*8]=~KEY[0];
+				5: assign HEX[0+j*8]=~KEY[0],
+							HEX[1+j*8]=~KEY[0],
+							HEX[6+j*8]=~KEY[0],
+							HEX[4+j*8]=~KEY[0],	
+							HEX[3+j*8]=~KEY[0],
+							HEX[7+j*8]=~KEY[0];
+			endcase
+		end
+endgenerate
+endmodule
+	
